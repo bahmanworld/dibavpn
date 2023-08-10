d083ff2ed056eda4008ef8c00567b95fbdf966c2
Bahman World <bahman.world@gmail.com>
2023-08-10 13:08:47 +0330

    some changes

--- pages/bahman.tsx
+++ pages/bahman.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { FiArrowLeft } from "react-icons/fi";
 
 const Bahman = () => {
   return (
-    <div>
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <div>bahman</div>
-      <Link href={"/"}>Home Page ↩</Link>
-    </div>
+      <Link href={"/"} className="flex flex-row items-center">
+        <span className="ml-1">
+          <FiArrowLeft />
+        </span>
+        <div>Home Page</div>
+      </Link>
+    </main>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-
 import Link from "next/link";
-
+import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <Link href={"/bahman"}>↪ Bahman Page</Link>
+      <div>
+        Home Page
+      </div>
+      <Link href={"/bahman"} className="flex flex-row items-center">
+        <div>
+          Bahman Page
+        </div>
+        <span className="ml-1">
+          <FiArrowRight />
+        </span>
+      </Link>
     </main>
   );
 }
